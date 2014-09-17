0956065adfd1c938bf133df63b612e6cd61873bc
Maurizio De Santis <desantis.maurizio@gmail.com>
2014-09-17 19:11:49 +0200

    implement pages#create action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,5 +7,18 @@ class PagesController < ApplicationController
   end
 
   def create
+    @page = Page.new(page_params)
+
+    if @page.save
+      redirect_to @page, notice: 'Page was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def page_params
+    params.require(:page).permit(:title, :slug, :content)
   end
 end
