dc83d3a8906fd32ed3c8badc0e604979f5bb093c
Maurizio De Santis <desantis.maurizio@gmail.com>
2014-09-17 19:59:31 +0200

    auto set page slug by page title

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,9 +1,19 @@
 class Page < ActiveRecord::Base
-  validates_presence_of :slug
-  
+  validates_presence_of :title
+
+  before_save :update_slug
+
   def to_param
     return nil unless persisted?
 
     slug
+  end
+
+  private
+
+  def update_slug
+    self.slug = title.parameterize
+
+    true
   end
 end
