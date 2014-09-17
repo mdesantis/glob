4bbc3e11c9c20780f5b39d4315d2732f0e4121db
Maurizio De Santis <desantis.maurizio@gmail.com>
2014-09-17 20:11:20 +0200

    add unique constraint to pages slug

---
+++ db/migrate/20140917180947_add_unique_constraint_to_pages_slug.rb
@@ -0,0 +1,5 @@
+class AddUniqueConstraintToPagesSlug < ActiveRecord::Migration
+  def change
+    add_index :pages, :slug, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140917161114) do
+ActiveRecord::Schema.define(version: 20140917180947) do
 
   create_table "pages", force: true do |t|
     t.string   "title"
@@ -20,5 +20,7 @@ ActiveRecord::Schema.define(version: 20140917161114) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "pages", ["slug"], name: "index_pages_on_slug", unique: true
 
 end
